c2e1d0470fc71209e7c5cca8cf2298b6481cb7bd
Chandan <chandan@localhost.localdomain>
2021-06-26 03:57:29 +0530

    second commit

--- src/components/Jumble.js
+++ src/components/Jumble.js
@@ -10,49 +10,68 @@ class Jumble extends Component
         this.state ={
             puzzles: [],
             result: '',
-            word: '',
+            pword: '',
+            desc: '',
             count: 0,
-            rindex: 0
+            rindex: 0,
+            active: false
         }
         this.clickHandler = this.clickHandler.bind(this)
         this.nextHandler = this.nextHandler.bind(this)
     }
     componentDidMount(){
         axios.get('http://localhost:8080/puzzlesjumble')
-        .then(response => this.setState({puzzles: response.data}))
+        .then(response => this.setState({puzzles: response.data}, this.initpuzzle))
         .catch(error => console.log(error))
-
     }
     clickHandler(event){
-        alert('jjjj')
         this.setState({
-            result: this.result + event.target.innerText,
-            count: this.state.count +1
+            result:  this.state.result + event.target.innerText,
+            active: false
+        })
+        event.target.disabled = true
+        console.log(this.state.result)
+    }
+    nextHandler(){
+        this.setState({
+            count: this.state.count +1,
+            active: false
+        }, this.initpuzzle)
+    }
+    initpuzzle(){
+        this.setState({
+            result: '',
+            pword: this.state.puzzles[this.state.count].pword,
+            desc: this.state.puzzles[this.state.count].desc
         })
     }
-    nextHandler(event){
-        alert('jjjj')
-        this.setState({
-            count: 1
-        })
+
+    emptystr(len)
+    {
+        var str = ''
+        for(let i=0; i < len; i++) {
+            str = str + ' '
+        }
+        return str
     }
+
     render(){
-        const {puzzles, count} = this.state
+        const {puzzles, count, pword, desc, result, active} = this.state
         const wordbtn =[]
         const ansbtn =[]
         
         if(puzzles.length > 0)
-        {
-            for(let i=0; i < puzzles[count].pword.length; i++) {
-            wordbtn.push(<Button variant="primary" size="lg">{puzzles[count].pword[i]}</Button>)
-            ansbtn.push(<Button variant="primary" size="lg"> </Button>)
+        {   
+            for(let i=0; i < pword.length; i++) {
+            wordbtn.push(<Button onClick={this.clickHandler} disabled={active} variant="primary" size="lg">{pword[i]}</Button>)
+            ansbtn.push(<Button variant="primary" size="lg">{result[i]}</Button>)
             }
         }
         return(
             <Card style={{ width: '68rem' }}>
             <Card.Img variant="top" src="jumble.png" />
             <Card.Body>
-            <Card.Title>{puzzles.length ? puzzles[count].desc : null}</Card.Title>
+            <Card.Title>{puzzles.length ? desc : null}</Card.Title>
             {ansbtn}
             <br/><br/><br/><br/>
             {wordbtn}
@@ -60,7 +79,7 @@ class Jumble extends Component
 
             <Button onClick={this.clickHandler} variant="primary" size="lg">Submit</Button>
             <Button variant="primary" disabled = {true} size="lg">Hint</Button>
-            <Button OnClick={this.nextHandler} variant="primary" size="lg">Skip</Button>
+            <Button onClick={this.nextHandler} variant="primary" size="lg">Skip</Button>
             <br/> <br/>
             </Card.Body>    
             </Card>
